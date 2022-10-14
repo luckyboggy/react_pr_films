a3d5c374eb01975a7d80c5bacdfb93537aa7c1ee
luckyboggy <luckyboggy@gmail.com>
2022-10-14 23:50:02 +0500

    add_header_footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {Header} from './layout/Header';
+import {Footer} from './layout/Footer';
+import {Main} from './layout/Main';
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <main className="container content">
+            <p>huinya</p>
+        </main>
+    )
+}
+
+export {Main}
