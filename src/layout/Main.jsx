9d9be66f7751198f9259b79ad5cf4fa83f58a262
luckyboggy <luckyboggy@gmail.com>
2022-10-15 15:49:02 +0500

    add_movies

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,35 @@
-function Main() {
-    return (
-        <main className="container content">
-            <p>huinya</p>
-        </main>
-    )
+import React from "react";
+import { Movies } from '../components/Movies'
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=e53efa40&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+        const { movies } = this.state;
+
+        return (
+            <main className="container content">
+                {
+                    movies.length ? (
+                        <Movies movies={this.state.movies} />
+                    ) : (
+                        <h5>loading...</h5>
+                    )
+                }
+
+
+
+            </main>
+        )
+    }
 }
 
-export {Main}
+export { Main }
